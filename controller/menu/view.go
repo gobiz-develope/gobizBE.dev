86f3e76391dadff89206b943866f58e6cee6a267
Ichika354 <handa82624@gmail.com>
2024-10-18 07:01:38 +0700

    Menu id

--- controller/menu/view.go
+++ controller/menu/view.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func GetMenuByMarket(w http.ResponseWriter, r *http.Request) {
@@ -29,3 +30,27 @@ func GetMenuByMarket(w http.ResponseWriter, r *http.Request) {
 
 	json.NewEncoder(w).Encode(market.Menu)
 }
+
+func GetMenuByID(w http.ResponseWriter, r *http.Request) {
+	menuID := r.URL.Query().Get("menu_id")
+
+	// Konversi menuID dari string ke primitive.ObjectID
+	objectID, err := primitive.ObjectIDFromHex(menuID)
+	if err != nil {
+			http.Error(w, "Invalid menu ID format", http.StatusBadRequest)
+			return
+	}
+
+	// Cari menu berdasarkan menu_id dari database
+	var menu model.Menu
+	collection := config.Mongoconn.Collection("market") // Pastikan koleksi yang benar
+	err = collection.FindOne(context.TODO(), bson.M{"menu._id": objectID}).Decode(&menu)
+
+	if err != nil {
+			http.Error(w, "Menu not found", http.StatusNotFound)
+			return
+	}
+
+	json.NewEncoder(w).Encode(menu)
+}
+
