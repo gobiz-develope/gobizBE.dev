fa61dfbda0746a3b86b2cc88a296c0b672c038e5
Ichika354 <handa82624@gmail.com>
2024-10-18 07:06:07 +0700

    Model

--- model/market.go
+++ model/market.go
@@ -20,6 +20,20 @@ type Market struct {
 	ID         primitive.ObjectID `bson:"_id,omitempty" json:"id"`
 	MarketName string             `bson:"nama_toko" json:"nama_toko"`
 	Slug       string             `bson:"slug" json:"slug"`
-	Alamat     string             `bson:"alamat" json:"alamat"`
+	Alamat     Address             `bson:"alamat" json:"alamat"`
 	Menu       []Menu             `bson:"menu" json:"menu"`
+	CreatedAt   time.Time          `bson:"createdAt,omitempty" json:"createdAt,omitempty"`
+	UpdatedAt   time.Time          `bson:"updatedAt,omitempty" json:"updatedAt,omitempty"`
+}
+
+type Address struct {
+	ID          primitive.ObjectID `bson:"_id" json:"id,omitempty"`
+	UserID      primitive.ObjectID `bson:"user_id" json:"user_id,omitempty"`
+	Street      string             `bson:"street" json:"street,omitempty"`
+	Province    string             `bson:"province" json:"province,omitempty"`
+	City        string             `bson:"city" json:"city,omitempty"`
+	Description string             `bson:"description" json:"description,omitempty"`
+	PostalCode  string             `bson:"postal_code" json:"postal_code,omitempty"`
+	CreatedAt   time.Time          `bson:"createdAt,omitempty" json:"createdAt,omitempty"`
+	UpdatedAt   time.Time          `bson:"updatedAt,omitempty" json:"updatedAt,omitempty"`
 }
